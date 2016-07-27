7c8dc17016808c0573c25882703760df12b97cae
Elena <elena.vilimaite@gmail.com>
2016-07-27 11:41:29 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='varys',
+    version='1.0.0',
+    packages=['varys', 'varys.spiders'],
+    url='https://github.com/elena-vi/varys',
+    license='',
+    author='Elena, Alex, Joe, Oliver',
+    author_email='',
+    description='A search engine'
+)
