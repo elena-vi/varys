0892193563f73c9fb9258692147fedf926e177ab
Elena <elena.vilimaite@gmail.com>
2016-07-29 11:29:38 +0100

    postgres start

--- varys/database.py
+++ varys/database.py
@@ -1,15 +1,20 @@
-import rethinkdb as r
+import psycopg2
 
 class VarysDatabase(object):
 
-	def __init__(self):
-		print 'hey?'
+	# def __init__(self):
+	# 	print 'hey?'
 
-	def hey(thing):
-		print '-----------------'
-		print '-----------------'
-		print thing
-		print '-----------------'
-		print '-----------------'
+	def connect(self):
+		print r.connect('localhost', 28015).repl()
+		print r.dbList()
 
 		pass
+
+	def insert(self, url, title):
+		r.connect('localhost', 28015).repl()
+		pass
+
+
+from database import VarysDatabase
+VarysDatabase().connect()
--- varys/pipelines.py
+++ varys/pipelines.py
@@ -5,15 +5,23 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import rethinkdb as r
-import database
+from database import VarysDatabase
 
 
 class VarysPipeline(object):
 
 	def open_spider(self, spider):
-		r.connect('localhost', 28015).repl()
+		VarysDatabase().connect()
+		pass
 
 	def process_item(self, item, spider):
-		hey(item["link"])
-		r.db('items').table('links').insert({ 'url': item["link"], 'title': item["title"]}).run()
+		# VarysDatabase().hey("yes")
+		# r.db('items').table('links').insert({ 'url': item["link"], 'title': item["title"]}).run()
 		return item
+
+	def close_spider(self, spider):
+		print '-----------------'
+		print 'close_spider'
+		print '-----------------'
+
+		pass
