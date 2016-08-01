3ce33be8b53f8aac74d7139a781cd035af2d6ea9
a-mellor <alexander.mellor@gmail.com>
2016-08-01 13:31:19 +0100

    removed order by id

--- search_engine/app/models/webpage.rb
+++ search_engine/app/models/webpage.rb
@@ -8,13 +8,12 @@ class Webpage
 
   def self.do_search(query_string, query_from)
     return [] if query_string == ""
-  
+
     results = self.find_by_sql("SELECT id, title, description, url
                                 FROM webpages
                                 WHERE to_tsvector('english', title || description)
                                 @@ plainto_tsquery('english', '#{query_string}')
                                 OR    url LIKE '%#{query_string}%'
-                                ORDER BY id
                                 LIMIT 10 OFFSET #{query_from}")
 
     convert_results_to_objects(results)
