de18dd1aeb45ae058e1c5a07b9c8feb29cbd4cbd
Unalterable <diamond.oliver@gmail.com>
2016-08-01 13:59:38 +0100

    cleaner spider

--- tests/url_test.py
+++ tests/url_test.py
@@ -3,28 +3,28 @@ from varys.src.urlParser import Parser
 
 class UrlsTests(unittest.TestCase):
 
-  def test_Remove_Query(self):
+  def test_Without_Query(self):
     url = 'http://www.bbc.co.uk/news/example.html?boring'
-    noQuery = Parser(url).removeQuery()
-    self.assertEqual(noQuery, 'http://www.bbc.co.uk/news/example.html')
+    noQuery = Parser(url)
+    self.assertEqual(noQuery.withoutQuery(), 'http://www.bbc.co.uk/news/example.html')
   
   def test_Join_Path(self):
     domain = 'http://www.bbc.co.uk'
     path = '/sport/tennis/silly.htm'
     joined = Parser(path).joinDomain(domain)
-    self.assertEqual(joined, 'http://www.bbc.co.uk/sport/tennis/silly.htm')
+    self.assertEqual(joined.fullUrl(), 'http://www.bbc.co.uk/sport/tennis/silly.htm')
 
   def test_Join_Path2(self):
     domain = 'http://www.bbc.co.uk'
     path = 'http://wrong.com/sport/tennis/silly.htm'
     joined = Parser(path).joinDomain(domain)
-    self.assertEqual(joined, 'http://www.bbc.co.uk/sport/tennis/silly.htm')
+    self.assertEqual(joined.fullUrl(), 'http://www.bbc.co.uk/sport/tennis/silly.htm')
 
   def test_Join_Path3(self):
     domain = 'http://www.bbc.co.uk/wrong_path?bad=query#silly_frag'
     path = '/sport/tennis/silly.htm'
     joined = Parser(path).joinDomain(domain)
-    self.assertEqual(joined, 'http://www.bbc.co.uk/sport/tennis/silly.htm')
+    self.assertEqual(joined.fullUrl(), 'http://www.bbc.co.uk/sport/tennis/silly.htm')
 
   def test_Approve_Link(self):
     url = 'http://www.bbc.co.uk/news/example.html?boring'
--- varys/spiders/CrawlSpider.py
+++ varys/spiders/CrawlSpider.py
@@ -1,21 +1,48 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
+from scrapy import Request
 from varys.items import VarysItem
 
 from varys.src.urlParser import Parser
 
 class CrawlSpider(CrawlSpider):
     name = "varys"
-    start_urls = ["http://makersacademy.com"]
-
-    rules = (
-        Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('//a',)), callback="parse_items", follow= True),
+    allowed_domains = ["bbc.co.uk"]
+    start_urls = (
+        'http://news.bbc.co.uk/',
     )
 
-    def parse_items(self, response):
-        hxs = HtmlXPathSelector(response)
-        item = VarysItem()
-        item["link"] = response.url
-        item["title"] = response.xpath("//title/text()").extract()
-        return(item)
+#rules = (
+#        Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=('//a',)), callback="parse_items", follow= True),
+#    )
+
+    def parse(self, response):
+      for newRequest in self.newRequests(response):
+          yield newRequest
+      for newItem in self.newItems(response):
+          yield newItem
+      
+      
+
+
+    def newRequests(self, response):
+      array = []
+
+      for link in response.xpath('//a/@href').extract():
+        parsed = Parser(link).joinDomain(response.url)
+        if (parsed.approved):
+          array.append( Request(parsed.fullUrl(), callback=self.parse) )
+      
+      return array
+
+
+    def newItems(self, response):
+      array = []
+
+      item = VarysItem()
+      item["link"] = response.url
+      item["title"] = response.xpath("//title/text()").extract()
+      array.append(item)
+
+      return array
