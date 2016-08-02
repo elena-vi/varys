4dbcdfade26b62e0d580766b57438c3d51322f8d
joestephens <joe.stephens@live.co.uk>
2016-08-02 21:20:30 +0100

    removed p

--- search_engine/app/models/webpage.rb
+++ search_engine/app/models/webpage.rb
@@ -10,13 +10,17 @@ class Webpage
   def self.do_search(query_string, query_from)
     return [] if query_string == ""
 
-    # url_query = query_string.split(' ').length > 1 ? "OR url LIKE '%#{query_string}%'" : ""
-
     results = self.find_by_sql("SELECT id, title, description, url, ts_rank_cd(textsearch, query) AS rank
                                 FROM webpages, plainto_tsquery('english', '#{query_string}') query, to_tsvector(url || title || description) textsearch
                                 WHERE query @@ textsearch
                                 ORDER BY rank DESC
                                 LIMIT 10 OFFSET #{query_from}")
+
+    # need to add function for checking if result is query homepage (if query string has no spaces)
+    #
+    # popularity table?
+    #
+    # loop through database again for both and adjust ranking accordingly
 
     convert_results_to_objects(results)
   end
