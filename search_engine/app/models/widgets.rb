49530c5af5a29540a4cb348c626a3e8436fe1ce9
a-mellor <alexander.mellor@gmail.com>
2016-08-03 13:05:56 +0100

    refactoring widget

--- search_engine/app/models/widgets.rb
+++ search_engine/app/models/widgets.rb
@@ -2,40 +2,56 @@ require_relative 'result.rb'
 
 class Widgets
 
-  def self.do_widget_search(query_string)
-    widgets = {}
+  SOURCES = { tube:
+                [
+                  "https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status",
+                  Proc.new { |query| query.include?('tube') }
+                ],
+              wikipedia:
+                [
+                  "https://en.wikipedia.org/w/api.php?action=opensearch&search=%s&limit=1&namespace=0&format=json",
+                  Proc.new { true }
+                ]
+            }
 
-    actions = {
-      tube: do_tube_search,
-      underground: do_tube_search,
-      status: do_tube_search
-    }
+  def self.all(query_string)
+    actions = {}
 
-    actions.each do |key, value|
-      widgets[key] = value if query_string.include?(key.to_s)
+    SOURCES.each do |widget, (url, condition)|
+      actions[widget] = Widgets.new(widget, query_string).get if condition.call(query_string)
     end
 
-    widgets[:wikipedia] = do_wikipedia_search(query_string)
+    # map = SOURCES.map do |widget, (url, condition)|
+    #   [(widget.to_sym) => Widgets.new(widget, query_string).get]
+    # end.compact
+    #
+    # p '--------------map'
+    # puts map
+    #
+    # p '---------------actions'
+    # puts actions
 
-    widgets
+    actions
+  end
+
+  def initialize(widget, query_string)
+    @widget = widget
+    @url = SOURCES[widget][0] % query_string
+  end
+
+  def get
+    response = RestClient::Request.execute(:url => @url, :method => :get, :verify_ssl => false)
+    prettify_json(@widget, JSON.parse(response))
   end
 
   private
 
-  def self.do_wikipedia_search(query_string)
-    return [] if query_string == ""
-    url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=#{query_string}&limit=1&namespace=0&format=json"
-    response = RestClient.get(url)
-    prettify_wikipedia_json(JSON.parse(response))
+  def prettify_json(widget, json)
+    return prettify_tube_json(json) if widget == :tube
+    return prettify_wikipedia_json(json) if widget == :wikipedia
   end
 
-  def self.do_tube_search
-    url = "https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status"
-    response = RestClient::Request.execute(:url => url, :method => :get, :verify_ssl => false)
-    do_pretty_tube(JSON.parse(response))
-  end
-
-  def self.do_pretty_tube(json_results)
+  def prettify_tube_json(json_results)
     result = []
 
     json_results.each do |line|
@@ -45,7 +61,7 @@ class Widgets
     result
   end
 
-  def self.prettify_wikipedia_json(json)
+  def prettify_wikipedia_json(json)
     description = json[2][0] ? json[2][0] : ""
     output = [Result.new(title: json[1][0], url: json[3][0], description: description)]
 
