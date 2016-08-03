45c3f8705c0d39f19b4a0d0c8e900d5a7767f332
Elena <elena.vilimaite@gmail.com>
2016-08-03 16:36:08 +0100

    no more orm

--- search_engine/app/models/webpage.rb
+++ search_engine/app/models/webpage.rb
@@ -2,34 +2,20 @@ class Webpage
 
   require 'pg'
 
-  # include DataMapper::Resource
+  attr_reader :id, :title, :description, :url, :rank
 
-  # property :id, Serial
-  # property :title, String, length: 255, required: true
-  # property :description, String, length: 255, required: true
-  # property :url, String, length: 255, required: true
-  # property :rank, String
-
-  attr_reader :title, :description, :url, :rank
-
-  def initialize(title, url, description, rank)
-    @title = title
-    @url = url
-    @description = description
-    @rank = rank
+  def initialize(params)
+    @id = params.fetch(:id, "")
+    @title = params.fetch(:title, "")
+    @url = params.fetch(:url, "")
+    @description = params.fetch(:description, "")
+    @rank = params.fetch(:rank, "")
   end
 
   def self.do_search(query_string, query_from)
     return [] if query_string == ""
 
-    # results = self.find_by_sql("SELECT id, title, description, url, ts_rank_cd(textsearch, query) AS rank
-    #                             FROM webpages, plainto_tsquery('english', '#{query_string}') query, to_tsvector(url || title || description) textsearch
-    #                             WHERE query @@ textsearch
-    #                             ORDER BY rank DESC
-    #                             LIMIT 10 OFFSET #{query_from}")
-
     begin
-      # results.clear if results
       connection = PG.connect :dbname => 'varys_' + ENV['RACK_ENV']
       results = connection.exec "SELECT id, title, description, url, ts_rank_cd(textsearch, query) AS rank
       FROM webpages, plainto_tsquery('english', '#{query_string}') query, to_tsvector(url || title || description) textsearch
@@ -41,9 +27,6 @@ class Webpage
       results = []
     ensure
       connection.close if connection
-      print '---------------'
-      print "CLOSE"
-      print "---------------\n"
     end
 
     # need to add function for checking if result is query homepage (if query string has no spaces)
@@ -51,11 +34,21 @@ class Webpage
     # popularity table?
     #
     # loop through database again for both and adjust ranking accordingly
-    
-    print "---------------"
-    print results
-    print "---------------\n"
+
     convert_results_to_objects(results)
+  end
+
+  def save!
+
+    begin
+      connection = PG.connect :dbname => 'varys_' + ENV['RACK_ENV']
+      connection.exec "INSERT INTO webpages (title, description, url) VALUES('#{self.title}','#{self.description}','#{self.url}')"
+    rescue PG::Error => e
+      puts e.message
+    ensure
+      connection.close if connection
+    end
+
   end
 
   private
@@ -63,7 +56,12 @@ class Webpage
   def self.convert_results_to_objects(query_results)
     results = []
     query_results.map do |result|
-      results << Webpage.new(result['title'], result['url'], result['description'], result['rank'])
+      results << Webpage.new( id: result['id'],
+      title: result['title'],
+      url: result['url'],
+      description: result['description'],
+      rank: result['rank']
+      )
     end
     results
   end
