e77c310e399d4194c0f4d476bd8e8533f950b4eb
Elena <elena.vilimaite@gmail.com>
2016-08-03 16:38:16 +0100

    factory girl re-write

--- search_engine/spec/factories/webpage.rb
+++ search_engine/spec/factories/webpage.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
-  factory :webpage do
+  factory :webpage, class: Webpage do
+  	id "1"
     title 'Portsmouth News: The News'
     description 'news, sport, business, lifestyle and more,
-                 from Portsmouth\'s newspaper, The News.'
+                 from newspaper from Portsmouth, The News.'
     url 'http://www.portsmouth.co.uk'
+
+    initialize_with { new(:id =>  id, :title => title, :description =>  description, :url =>  url) }
   end
 end
