042fa0041e86bc3527e433ef4d29cb7c89df5df2
a-mellor <alexander.mellor@gmail.com>
2016-08-03 17:08:47 +0100

    refactored widget. refactored css for tube layout

--- search_engine/app/models/widgets.rb
+++ search_engine/app/models/widgets.rb
@@ -2,56 +2,52 @@ require_relative 'result.rb'
 
 class Widgets
 
-  SOURCES = [ {name: 'tube',
-               url: "https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status",
-               condition: Proc.new { |query| query.include?('tube') }
+  SOURCES = [ { name: 'tube',
+                url: "https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status",
+                before_condition: Proc.new { |query|
+                  query.include?('tube')
+                },
+                after_condition: Proc.new { |json|
+                  true
+                },
+                parse_json: Proc.new { |json|
+                  json.map do |line|
+                    { id: line["id"], name: line["name"], status: line["lineStatuses"][0]["statusSeverityDescription"], reason: line["lineStatuses"][0]["reason"] }
+                  end
+                }
               },
-              {name: 'wikipedia',
-               url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=%s&limit=1&namespace=0&format=json",
-               condition: Proc.new { true }
+              { name: 'wikipedia',
+                url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=%s&limit=1&namespace=0&format=json",
+                before_condition: Proc.new { |query|
+                  true
+                },
+                after_condition: Proc.new { |json|
+                  !json.first.description.empty? && !json.first.description.include?('may refer to')
+                },
+                parse_json: Proc.new { |json|
+                  [Result.new(title: json[1][0], url: json[3][0], description: json[2][0] || "")]
+                }
               }
             ]
 
   def self.all(query)
-    SOURCES.select{ |widget| widget[:condition].call(query) }.map do |widget|
-      [widget[:name].to_sym, Widgets.new(widget[:name].to_sym, widget[:url], query).get]
-    end.to_h
+    SOURCES.map { |widget| [widget[:name].to_sym, Widgets.new(widget, query).get] }.to_h.delete_if{ |k,v| !v }
   end
 
-  def initialize(widget, url, query)
-    @widget = widget
-    @url = url % query
+  def initialize(widget, query)
+    @name = widget[:name]
+    @url = widget[:url] % query
+    @query = query
+    @before_condition = widget[:before_condition]
+    @after_condition = widget[:after_condition]
+    @parse_json = widget[:parse_json]
   end
 
   def get
+    return nil unless @before_condition.call(@query)
     response = RestClient::Request.execute(:url => @url, :method => :get, :verify_ssl => false)
-    prettify_json(@widget, JSON.parse(response))
-  end
-
-  private
-
-  def prettify_json(widget, json)
-    return prettify_tube_json(json) if widget == :tube
-    return prettify_wikipedia_json(json) if widget == :wikipedia
-  end
-
-  def prettify_tube_json(json_results)
-    result = []
-
-    json_results.each do |line|
-      result << { id: line["id"], name: line["name"], status: line["lineStatuses"][0]["statusSeverityDescription"], reason: line["lineStatuses"][0]["reason"]}
-    end
-
-    result
-  end
-
-  def prettify_wikipedia_json(json)
-    description = json[2][0] ? json[2][0] : ""
-    output = [Result.new(title: json[1][0], url: json[3][0], description: description)]
-
-    output_description = output.first.description
-    return [] if output_description.include? 'may refer to'
-
-    output
+    json = @parse_json.call(JSON.parse(response))
+    return nil unless @after_condition.call(json)
+    json
   end
 end
